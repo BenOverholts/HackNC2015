62f7a93bcbc7d668d92884b8245efa5696bb5488
Vikash <vsahu24@gmail.com>
2015-10-11 06:30:11 -0400

    progress bar

--- sparkVPN/routes/index.js
+++ sparkVPN/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 var TOKEN = '21ad0aa08d9d33af5b6ae68edb97b74e6edd18aca1f0ad18b9ea96df5f3a245d';
+var dropletName = 1;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +18,7 @@ router.post('/', function(req, res) {
 		url: 'https://api.digitalocean.com/v2/droplets',
 		method: 'POST',
 		json: {
-			name: "example.com",
+			name: dropletName++,
   			region: "nyc3",
   			size: "512mb",
   			image: "ubuntu-14-04-x64",
@@ -48,12 +50,6 @@ router.post('/', function(req, res) {
 				var ipv4 = JsonBody.droplet.networks.v4[0].ip_address;
 				console.log(ipv4);
 		})}, 70000);*/
-		res.setHeader('Content-disposition', 'attachment; filename=theDocument.txt');
-		res.setHeader('Content-type', 'text/plain');
-		res.charset = 'UTF-8';
-		res.write("Hello, world");
-		res.end();
-
 	})
 });
 	
